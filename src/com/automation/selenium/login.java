ec4cda72378641902d2c6f6cd41e23c1ff22f37a
ashutosh.verma1 <ashutosh.verma1@timesinternet.in>
2020-04-29 15:28:36 +0530

    revamp login page

--- src/com/automation/selenium/login.java
+++ src/com/automation/selenium/login.java
@@ -1,13 +1,30 @@
 package com.automation.selenium;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 public class login {
 
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "/Users/ashutosh.verma1/Documents/Selenium/chromedriver");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com/");
+	public WebDriver driver;
+
+	@Test(priority = 1)
+	public void launchBrowser() {
+		System.out.println("launching firefox browser");
+		System.setProperty("webdriver.gecko.driver", "/Users/ashutosh.verma1/Documents/Selenium/geckodriver");
+		driver = new FirefoxDriver();
 	}
+
+	@Test(priority = 2)
+	public void openApplication() {
+		driver.navigate().to("http://www.google.com");
+	}
+
+	@Test(priority = 3)
+	public void closeDriver() {
+		if (driver != null) {
+			driver.close();
+		}
+	}
+
 }
